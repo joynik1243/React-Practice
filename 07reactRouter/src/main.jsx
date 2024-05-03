e36657117834df5a216b7bd16aff5bd40178d469
joynik1243 <joynikub6@gmail.com>
2024-05-04 00:56:19 +0530

    React Router Practice 2

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -5,12 +5,23 @@ import './index.css'
 import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from "./Layout.jsx";
 import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx";
+import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route path="" element= {<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user/" element={<User />}>
+        <Route path=":userId" />
+      </Route>
+
+
+      <Route path="*" element={<div>404 Not Found</div>} />
     </Route>
   )
 )
